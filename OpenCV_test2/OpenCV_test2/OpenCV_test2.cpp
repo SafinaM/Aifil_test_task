1b206ed34cc0d48118b398cf1614cdb5272e8297
SafinaM <tigra_84@mail.ru>
2016-04-03 23:31:34 +0300

    Программа для нахождения мест склейки видео-файлов. 1. Прилагается xml-файл my_histogram_file_temp.xml с цветовыми гистограмами кадров - образцов. 2. Результаты выводятся на экран и сохраняются в файл "results.txt". 3. Можно корректировать поведение алгоритма добавлением образцов при просмотре видео, по умолчанию кадры записываются в последовательность черных кадров. Опция требует доработки (для выбора нужной последовательсти вручную после сохранения нужного кадра). 4. Можно сохранить все изменения в файл xml. По умолчанию все изменения, сделанные во время просмотра видео не сохранятся, для этого надо запустить функцию saveNewDataForBasicFrames(). 5. Видео надо поместить в корневую папку. Для выбора пути, необходимо доработать нужную опцию. 6. В отдельном окне показывается гистограмма текущего видео. 7. Для быстрого оценивания работы алгоритма можно перемотать видео вперед. 8. Алгоритм работает на основе цветовых гистограмм. 9. Единичные срабатывания на отдельных кадрах видео не учитываются.
    Program for finding borders in video file.
    The xml-file is adding to the program. The results are showing on the screen and saving to the computer in file "results.txt".
    It is possible to teach program to recognize necessary template frames on borders.
    By default the template frames are adding to "black" sequence.
    It is possible to save changes in templates in xml-file. By default changing will not happend.
    Результы работы алгоритма приведены ниже.
    Для первого видео:
    Frame numbers 1) 1 - 62
    Frame numbers 2) 3638 - 3839
    Frame numbers 3) 7390 - 7625
    Frame numbers 4) 11196 - 11447
    Frame numbers 5) 14882 - 15139
    Frame numbers 6) 18691 - 18906
    Для второго:
    Frame numbers 1) 1 - 81
    Frame numbers 2) 3641 - 3836
    Frame numbers 3) 7407 - 7620
    Frame numbers 4) 11195 - 11452
    Frame numbers 5) 14993 - 15287
    Frame numbers 6) 18835 - 19101
    Для третьего:
    Frame numbers 1) 1 - 81
    Frame numbers 2) 3641 - 3836
    Frame numbers 3) 7407 - 7620
    Frame numbers 4) 11195 - 11452
    Frame numbers 5) 14993 - 15287
    Frame numbers 6) 18835 - 19101

--- OpenCV_test2/OpenCV_test2/OpenCV_test2.cpp
+++ OpenCV_test2/OpenCV_test2/OpenCV_test2.cpp
@@ -6,6 +6,7 @@
 //This algorithm will not be ideal, but it is will be very simple, and I hope working.
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -14,142 +15,222 @@
 using namespace cv;
 using namespace std;
 
-
-Mat frame;
-Mat image;
-vector <Mat> matHist;
-vector <Mat> matHistB;
-Mat H1;
-Mat H3;
-Mat H2;
-int slider_pos = 0;
-VideoCapture cap("test3.mp4"); // open the video file for reading
-Mat showColorHist(const Mat &src)  //function of color histrogram forming
+struct Edge
 {
-	//vector<Mat> bgr_planes;  // Separate the image in 3 places ( B, G and R )
-	//split(temp, bgr_planes);
+	int x = 0;
+	int y = 0;
+};
+class CutBorders
+{
+private:
+	Mat frame;
+	Mat image;
+	vector <Mat> matHist;
+	vector <Mat> matHistB;
+	Mat H1;                      //value for histogram of reference sequence
+	Mat H2;                      //histogram of video file frames
+	vector <Edge> edges;         //vector with borders of the input video file
+	int L, R;                    //temporary values for left and right sides of range
+	
+public:
+	VideoCapture *cap;
+    int slider_pos;
+	
+	CutBorders()
+	{
+		slider_pos = 0;
+		string temp;
+		cout << "Enter the name of video file from the root directory: ";  //for example test1.mp4
+		cin >> temp;
+		cap = new VideoCapture(temp);
+	}
+	
+	auto getHist(const Mat &src)
+	{
+		Mat hsv;
+		cvtColor(src, hsv, COLOR_BGR2HSV);
+		// Quantize the hue to 30 levels
+		// and the saturation to 32 levels
+		int hbins = 30, sbins = 32;
+		int histSize[] = { hbins, sbins };
+		// hue varies from 0 to 179, see cvtColor
+		float hranges[] = { 0, 180 };
+		// saturation varies from 0 (black-gray-white) to
+		// 255 (pure spectrum color)
+		float sranges[] = { 0, 256 };
+		const float* ranges[] = { hranges, sranges };
+		MatND hist;
+		// we compute the histogram from the 0-th and 1-st channels
+		int channels[] = { 0, 1 };
+		calcHist(&hsv, 1, channels, Mat(), hist, 2, histSize, ranges, true, false);
+		return hist;
+	}
 
-	///// Establish the number of bins
-	//int histSize = 256;
+	auto showColorHist(const Mat &src)  //function of color histrogram forming
+	{
+		Mat hsv;
+		int hbins = 30, sbins = 32;
 
-	///// Set the ranges ( for B,G,R) )
-	//float range[] = { 0, 256 };
-	//const float* histRange = { range };
+		MatND hist = getHist(src);
 
-	//bool uniform = true; bool accumulate = false;
+		double maxVal = 0;
+		minMaxLoc(hist, 0, &maxVal, 0, 0);
+		int scale = 10;
+		Mat histImg = Mat::zeros(sbins*scale, hbins * 10, CV_8UC3);
+		for (int h = 0; h < hbins; h++)
+			for (int s = 0; s < sbins; s++)
+			{
+				float binVal = hist.at<float>(h, s);
+				int intensity = cvRound(binVal * 255 / maxVal);
+				rectangle(histImg, Point(h*scale, s*scale),
+					Point((h + 1)*scale - 1, (s + 1)*scale - 1),
+					Scalar::all(intensity),
+					CV_FILLED);
+			}
+		namedWindow("H-S Histogram", 1);
+		imshow("H-S Histogram", histImg);
+		return hist;
+	}
 
-	//Mat b_hist, g_hist, r_hist;
-
-	///// Compute the histograms:
-	//calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
-	//calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
-	//calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
-
-	//// Draw the histograms for B, G and R
-	//int hist_w = 512; int hist_h = 400;
-	//int bin_w = cvRound((double)hist_w / histSize);
-
-	//Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
-
-	///// Normalize the result to [ 0, histImage.rows ]
-	//normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	//normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	//normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-
-	///// Draw for each channel
-	//for (int i = 1; i < histSize; i++)
-	//{
-	//	line(histImage, Point(bin_w*(i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
-	//		Point(bin_w*(i), hist_h - cvRound(b_hist.at<float>(i))),
-	//		Scalar(255, 0, 0), 2, 8, 0);
-	//	line(histImage, Point(bin_w*(i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
-	//		Point(bin_w*(i), hist_h - cvRound(g_hist.at<float>(i))),
-	//		Scalar(0, 255, 0), 2, 8, 0);
-	//	line(histImage, Point(bin_w*(i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
-	//		Point(bin_w*(i), hist_h - cvRound(r_hist.at<float>(i))),
-	//		Scalar(0, 0, 255), 2, 8, 0);
-	//}
-
-	Mat hsv;
-
-	cvtColor(src, hsv, COLOR_BGR2HSV);
-	// Quantize the hue to 30 levels
-	// and the saturation to 32 levels
-	int hbins = 30, sbins = 32;
-	int histSize[] = { hbins, sbins };
-	// hue varies from 0 to 179, see cvtColor
-	float hranges[] = { 0, 180 };
-	// saturation varies from 0 (black-gray-white) to
-	// 255 (pure spectrum color)
-	float sranges[] = { 0, 256 };
-	const float* ranges[] = { hranges, sranges };
-	MatND hist;
-	// we compute the histogram from the 0-th and 1-st channels
-	int channels[] = { 0, 1 };
-	calcHist(&hsv, 1, channels, Mat(), // do not use mask
-		hist, 2, histSize, ranges,
-		true, // the histogram is uniform
-		false);
-	double maxVal = 0;
-	minMaxLoc(hist, 0, &maxVal, 0, 0);
-	int scale = 10;
-	Mat histImg = Mat::zeros(sbins*scale, hbins * 10, CV_8UC3);
-	for (int h = 0; h < hbins; h++)
-		for (int s = 0; s < sbins; s++)
+	void initDataFromFile()
+	{
+		FileStorage out("my_histogram_file.xml", cv::FileStorage::READ);         //downloading results from xml-file
+		if (!out.isOpened())
 		{
-			float binVal = hist.at<float>(h, s);
-			int intensity = cvRound(binVal * 255 / maxVal);
-			rectangle(histImg, Point(h*scale, s*scale),
-				Point((h + 1)*scale - 1, (s + 1)*scale - 1),
-				Scalar::all(intensity),
-				CV_FILLED);
+			cout << "Unable to open file storage!" << endl;
 		}
-	//	namedWindow("Source", 1);
-	//	imshow("Source", src);
-	namedWindow("H-S Histogram", 1);
-	imshow("H-S Histogram", histImg);
+		matHist.clear();
+		matHistB.clear();
+		int frameCount = (int)out["myHist"];
+		out["myHist"] >> matHist;         //the vector with the histogrames of white and mixed frames
+		out["myHist"] >> matHistB;
+		out.release();
+	}
 
-	return hist;
+	void saveNewDataForBasicFrames()
+	{
+		FileStorage fs("my_histogram_file.xml", cv::FileStorage::WRITE);
+		if (!fs.isOpened())
+		{
+			cout << "unable to open file storage!" << endl;
+		}
+		fs << "myHist" << matHist;
+		fs << "myHistB" << matHistB;
+		fs.release();
+	}
 
-	//static int count(0);
-	//stringstream ss;
-	//ss << count;
-	//string countStr = ss.str();
-	//string imageName = "hist_" + countStr;
-	//string FullPath = imageName + ".jpg";// "D:\\VS2015\\Projects\\OpenCV_test2\\" +
-	//imwrite(FullPath, histImg);      //saving histograms
-	//cout << " image has been saved " << std::endl;
-	//++count;
+	bool comparingHists()
+	{
+		
+		if (!matHist.empty())
+		{
+			int k = 0;
+			for (auto it : matHist)
+			{
+				k++;
+				if (compareHist(it, H2, HISTCMP_CHISQR_ALT) < 800000)
+				{
+					cout << compareHist(it, H2, HISTCMP_CHISQR_ALT) << " 1 " << k << endl;				
+					return true;
+				}
+			}
+		}
+		if (!matHistB.empty())
+		{
+			int k = 0;
+			for (auto it : matHistB)
+			{
+				k++;
+				if (compareHist(it, H2, HISTCMP_CHISQR_ALT) < 300000)
+				{
+	    			cout  << compareHist(it, H2, HISTCMP_CHISQR_ALT) << " 2 " << k << endl;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-	// Display
-	//	namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE);
-	//	imshow("calcHist Demo", histImg);
-}
-auto getHist(const Mat &src)
-{
-	Mat hsv;
-	cvtColor(src, hsv, COLOR_BGR2HSV);
-	// Quantize the hue to 30 levels
-	// and the saturation to 32 levels
-	int hbins = 30, sbins = 32;
-	int histSize[] = { hbins, sbins };
-	// hue varies from 0 to 179, see cvtColor
-	float hranges[] = { 0, 180 };
-	// saturation varies from 0 (black-gray-white) to
-	// 255 (pure spectrum color)
-	float sranges[] = { 0, 256 };
-	const float* ranges[] = { hranges, sranges };
-	MatND hist;
-	// we compute the histogram from the 0-th and 1-st channels
-	int channels[] = { 0, 1 };
-	calcHist(&hsv, 1, channels, Mat(), hist, 2, histSize, ranges, true, false);
-	return hist;
-}
+	void findBorders()
+	{
+		int fWidth = (int)cap->get(CV_CAP_PROP_FRAME_WIDTH);
+		int fHeight = (int)cap->get(CV_CAP_PROP_FRAME_HEIGHT);
+		VideoWriter out_cap("result1.avi", CV_FOURCC('M', 'P', '4', '2'), 30, Size(fWidth, fHeight));
+		int nFrames = (int)cap->get(CV_CAP_PROP_FRAME_COUNT);
+		while (true)
+		{
+			bool bSuccess = cap->read(frame); // read a new frame from video
 
-static void onMouse(int event, int x, int y, int, void*)  //getting the frame from left button press
-{
-	static int count(0);
-	if (event == EVENT_LBUTTONDOWN)
+			if (!bSuccess) //if not success, break loop
+			{
+				cout << "Cannot read the frame from video file" << endl;
+				break;
+			}
+			H2 = showColorHist(frame);  //showing of current frame's histogram in separate window
+			int iFrame = (int)cap->get(CV_CAP_PROP_POS_FRAMES);        //number of current frame
+			
+			if (comparingHists())      //extracting range from separate points, all digits - frame numbers
+			{
+				int temp = (int)cap->get(CV_CAP_PROP_POS_FRAMES);       //number of current frame of  border
+				if (L == 0)        
+				{
+					L = temp;                  
+				}
+				else if (temp - L >= 30 && L != 0)
+				{
+					R = temp;
+				}
+				
+				Point center(250, 250);
+				circle(frame, center, 30, Scalar(0, 0, 255), -1);
+				temp = 0;
+			}
+			if ( R == 0 && L != 0 && iFrame - L >= 160 )   //160 frames = 8 seconds
+			{
+				L = 0;
+			}
+			if (R - L >= 400)
+			{
+				R = 0;
+				L = 0;
+			}
+			
+			if (R != 0 && L != 0 && iFrame - L >= 300)
+			{
+				Edge *p = new Edge;
+				int deltaL = 40;
+				int deltaR = 20;
+
+				if (L - deltaL > 1)
+					L -= deltaL;
+				else
+					L = 1;
+				p->x = L;
+
+				if (R + deltaR < nFrames)
+					R += deltaR;
+				else
+					R = nFrames;
+				p->y = R;
+
+				edges.push_back(*p);       //vector with frame ranges 
+				showResults();             //show results on screen
+				L = 0;
+				R = 0;
+			}
+
+			imshow("Video 1 for task from Aifil", (frame)); //show the frame in "Video 1 for task from Aifil" window
+			cvSetTrackbarPos("Slider", "Video 1 for task from Aifil", ++slider_pos);
+		
+			if (waitKey(1) == 27) //waiting for 'esc' key press for 30 ms. If 'Esc' key is pressed, break loop
+			{
+				cout << "Esc key is pressed by user" << endl;
+				exit(0);
+			}
+		}
+	}
+
+	void workWithMouse(int count)
 	{
 		stringstream ss;
 		ss << count;
@@ -160,136 +241,79 @@ static void onMouse(int event, int x, int y, int, void*)  //getting the frame fr
 		cout << " image has been saved " << std::endl;
 		++count;
 		image = imread(FullPath, 1);
-		H1 = getHist(image);  //!!! was frame get historfram of definite frame
-		matHist.push_back(H1);
+		H1 = getHist(frame);  //!!! get histogram of definite frame
+		matHistB.push_back(H1);
 		Sleep(100);
-		//		namedWindow("Frame", CV_WINDOW_AUTOSIZE);
-		//		imshow("Frame", image);
 		showColorHist(image);
 	}
-}
-void formMatHistFromFiles()
-{
-	int nImages = 69;
-	int count = 1;
-	stringstream ss;
-	ss << count;
-	for (int i = 1; i < nImages; i++)
-	{
-		Mat temp;
-		string countStr = ss.str();
-		string imageName = "im_" + countStr;
-		string FullPath = imageName + ".jpg";// "D:\\VS2015\\Projects\\OpenCV_test2\\" +
-		temp = imread(FullPath, 1);
-		H1 = getHist(temp);  //get historfram of definite frame
-		matHist.push_back(H1);
 
+	void showResults()
+	{
+		int i = 1;
+		for (auto it : edges)
+		{
+			cout << "Frame numbers " << i << ") " << it.x << " - " << it.y << endl;
+			i++;
+		}
+	}
+	void openFileSetTrackbar();
+
+	void saveResultsToFile()
+	{
+		ofstream in("Results.txt");
+
+		int i = 1;
+		for (auto it : edges)
+		{
+			in << "Frame numbers " << i << ") " << it.x << " - " << it.y << endl;
+			i++;
+		}
+	}
+};
+
+CutBorders cut;
+
+static void onMouse(int event, int x, int y, int, void*)
+{
+	static int count(0);
+	if (event == EVENT_LBUTTONDOWN)
+	{
+		cut.workWithMouse(count);
 	}
 }
 
 void on_trackbar(int, void*)
 {
-	int position = (double)slider_pos;
-	cap.set(CV_CAP_PROP_POS_FRAMES, position);
-	//imshow("",);
+	int position = cut.slider_pos;
+	cut.cap->set(CV_CAP_PROP_POS_FRAMES, position);
+}
+
+void CutBorders::openFileSetTrackbar()
+{
+	if (!cap->isOpened())  // if not success, exit program
+	{
+		cerr << "Cannot open the video file" << endl;
+		exit(1);
+	}
+	int nFrame = (int)cap->get(CV_CAP_PROP_FRAME_COUNT);
+//	cap->set(CV_CAP_PROP_POS_FRAMES, 12700);
+
+	namedWindow("Video 1 for task from Aifil", CV_WINDOW_AUTOSIZE); //create a window called "Video 1 for task from Aifil"
+	if (nFrame != 0)
+	{
+		createTrackbar("Position", "Video 1 for task from Aifil", &slider_pos, nFrame, on_trackbar);
+	}
+	setMouseCallback("Video 1 for task from Aifil", onMouse, 0);  //work with mouse
 }
 
 int main()
 {
-	
-	
-	if (!cap.isOpened())  // if not success, exit program
-	{
-		cout << "Cannot open the video file" << endl;
-		return -1;
-	}
-
-	//cap.set(CV_CAP_PROP_POS_MSEC, 300); //start the video at 300ms
-	int iFrame = (int)cap.get(CV_CAP_PROP_FRAME_COUNT);
-	
-	double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
-	cout << "Frame per seconds : " << fps << endl; //cout frames per seconds
-
-	namedWindow("Video 1 for task from Aifil", CV_WINDOW_AUTOSIZE); //create a window called "Video 1 for task from Aifil"
-	if (iFrame != 0)
-	{
-		createTrackbar("Position", "Video 1 for task from Aifil", &slider_pos, iFrame, on_trackbar);
-	}
-	setMouseCallback("Video 1 for task from Aifil", onMouse, 0);  //work with mouse
-	//formMatHistFromFiles();
-	
-	
-	FileStorage out("my_histogram_file.xml", cv::FileStorage::READ);
-	if (!out.isOpened())
-	{
-		cout << "unable to open file storage!" << endl;
-	}
-	matHist.clear();
-	int frameCount = (int)out["myHist"];
-
-	out["myHist"] >> matHist;
-
-	
-	
-	while (true)
-	{
-		bool bSuccess = cap.read(frame); // read a new frame from video
-
-		if (!bSuccess) //if not success, break loop
-		{
-			cout << "Cannot read the frame from video file" << endl;
-			break;
-		}
-		H2 = showColorHist(frame);
-		if (!matHist.empty())
-		{
-			for (auto x : matHist)
-			{
-				
-				if (compareHist(x, H2, HISTCMP_CHISQR_ALT) < 1000000)
-				{
-					cout << compareHist(x, H2, HISTCMP_CHISQR_ALT) << endl;
-					Point center(250, 250);
-	     			circle(frame, center, 30, Scalar(0, 0, 255), -1);
-				//	break;
-					//	Sleep(300);
-				}
-			}
-			
-
-			//	cout << compareHist(H1, H2, CV_COMP_CHISQR) << endl;
-			//	cout << compareHist(H1, H2, CV_COMP_BHATTACHARYYA) << endl;
-			//	cout << compareHist(H1, H2, CV_COMP_INTERSECT) << endl;
-			//	Sleep(200);
-
-		}
-		//Sleep(100);
-		
-		imshow("Video 1 for task from Aifil", (frame)); //show the frame in "Video 1 for task from Aifil" window
-		cvSetTrackbarPos("Slider", "Video 1 for task from Aifil", ++slider_pos);
-
-		if (waitKey(30) == 35) //waiting for 'esc' key press for 30 ms. If 'Esc' key is pressed, break loop
-		{
-			cout << "Esc key is pressed by user" << endl;
-			break;
-		}
-	}
-
-	/*FileStorage fs("my_histogram_file.xml", cv::FileStorage::WRITE);
-	if (!fs.isOpened())
-	{
-		cout << "unable to open file storage!" << endl;
-	}
-	
-		fs << "myHist" << matHist;
-	fs.release();*/
-
-	
-	
-	out.release();
-
-	
-	//	cin.get();
+	cut.initDataFromFile();
+	cut.openFileSetTrackbar();
+	cut.findBorders();
+//	cut.saveNewDataForBasicFrames(); //by default write to array with the "black frames"
+	cut.showResults();
+	cut.saveResultsToFile();
+	cin.get();
 	return 0;
-
 }
---
+++ OpenCV_test2/OpenCV_test2/read_me_main.txt
@@ -0,0 +1,39 @@
+Программа для нахождения мест склейки видео-файлов. 
+1. Прилагается xml-файл my_histogram_file_temp.xml с цветовыми гистограмами кадров - образцов.
+2. Результаты выводятся на экран и сохраняются в файл "results.txt".
+3. Можно корректировать поведение алгоритма добавлением образцов при просмотре видео, по умолчанию кадры записываются в последовательность черных кадров. Опция требует доработки (для выбора нужной последовательсти вручную после сохранения нужного кадра).
+4. Можно сохранить все изменения в файл xml. По умолчанию все изменения, сделанные во время просмотра видео не сохранятся, для этого надо запустить функцию saveNewDataForBasicFrames().
+5. Видео надо поместить в корневую папку. Для выбора пути, необходимо доработать нужную опцию. 
+6. В отдельном окне показывается гистограмма текущего видео.
+7. Для быстрого оценивания работы алгоритма можно перемотать видео вперед.
+8. Алгоритм работает на основе цветовых гистограмм.
+9. Единичные срабатывания на отдельных кадрах видео не учитываются.
+
+Program for finding borders in video file. 
+The xml-file is adding to the program. The results are showing on the screen and saving to the computer in file "results.txt". 
+It is possible to teach program to recognize necessary template frames on borders. 
+By default the template frames are adding to "black" sequence. 
+It is possible to save changes in templates in xml-file. By default changing will not happend.
+Результы работы алгоритма приведены ниже.
+Для первого видео: 
+Frame numbers 1) 1 - 62
+Frame numbers 2) 3638 - 3839
+Frame numbers 3) 7390 - 7625
+Frame numbers 4) 11196 - 11447
+Frame numbers 5) 14882 - 15139
+Frame numbers 6) 18691 - 18906
+Для второго:
+Frame numbers 1) 1 - 81
+Frame numbers 2) 3641 - 3836
+Frame numbers 3) 7407 - 7620
+Frame numbers 4) 11195 - 11452
+Frame numbers 5) 14993 - 15287
+Frame numbers 6) 18835 - 19101
+Для третьего:
+Frame numbers 1) 1 - 81
+Frame numbers 2) 3641 - 3836
+Frame numbers 3) 7407 - 7620
+Frame numbers 4) 11195 - 11452
+Frame numbers 5) 14993 - 15287
+Frame numbers 6) 18835 - 19101
+
